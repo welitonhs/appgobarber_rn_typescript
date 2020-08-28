3def43c458ddc955325ccd6e9115c3c9ebd33e2d
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-08-28 00:54:26 -0300

    Added logo in project and created folder @types to validate PNG

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Text, Image } from 'react-native';
 
 import { Container } from './styles';
 
+import logoImg from '../../assets/logo.png';
+
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Text>teste</Text>
+    </Container>
+  );
 };
 
 export default SignIn;
