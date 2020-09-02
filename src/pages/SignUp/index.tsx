6400fb3f169ad30a0bfa10996ade1fe912ae52ac
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-09-02 00:37:11 -0300

    Maked style for page SignUp and added useNavigation to navigate between SignIn and SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua Conta</Title>
+            </View>
+            <Input name="Name" icon="lock" placeholder="Nome" />
+            <Input name="Email" icon="mail" placeholder="E-mail" />
+            <Input name="Password" icon="lock" placeholder="Password" />
+
+            <Button
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              Cadastrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignInButton
+        onPress={() => {
+          navigation.navigate('SignIn');
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#ff9000" />
+        <BackToSignInText>Fazer Login</BackToSignInText>
+      </BackToSignInButton>
+    </>
+  );
 };
 
 export default SignUp;
