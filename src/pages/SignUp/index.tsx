647028363720db0f00a762f47772c2f94a4c7eac
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-09-07 20:36:45 -0300

    Function to create acount in API working with axios

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -55,9 +56,9 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
-      // await api.post('/users', data);
-      // history.push('/');
+      await api.post('/users', data);
       Alert.alert('Cadastro realizado.', 'Você já pode fazer seu logon.');
+      navigation.navigate('SignIn');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
